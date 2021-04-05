3621d8081f8874ecf2b0532782dfe8672589c2da
Shun Liu <Shun.Liu@noaa.gov>
2021-04-05 20:16:27 +0000

    test.py

--- test.py
+++ test.py
@@ -10,11 +10,23 @@ from netCDF4 import Dataset
 pts = 1000
 np.random.seed(28041990)
 a = np.random.normal(0, 1, size=pts)
-b = np.random.normal(2, 1, size=pts)
-x = np.concatenate((a, b))
-plt.plot(a)
-k2, p = stats.normaltest(x)
-print(k2)
+print(stats.describe(a))
+
+b = np.random.normal(0, 2, size=pts)
+#x = np.concatenate((a, b))
+#k2, p = stats.normaltest(a)
+result = stats.ttest_1samp(a,0.041)
+
+x=range(0,1000)
+
+#fig=plt.figure(figsize=(8.0,7.5))
+#plt.scatter(x,a)
+#plt.savefig('han.png')
+
+print(result.statistic)
+print(result.pvalue)
+
+p=result.pvalue
 alpha = 1e-3
 print("p = {:g}".format(p))
 
