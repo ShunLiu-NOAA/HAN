3e609842c074390187f117f610f384e4bb9346b2
Shun Liu <Shun.Liu@noaa.gov>
2021-04-24 20:46:25 -0400

    han.sh

--- han.sh
+++ han.sh
@@ -1,6 +1,22 @@
-subtask=5
+#!/bin/bash -l
+
 OBSTYPE='rw'
-VarName='radial_velocity'
-taskdir=./data
-filename="ufo_${OBSTYPE}_output_2020101300_0000.nc4"
-python han.py $taskdir/$filename $OBSTYPE $VarName $subtask
+
+genyaml=true
+
+if [ $genyaml = "true" ]; then
+   echo "generating confg.yaml"
+fi
+
+cat > config.yaml <<EOF
+paths:
+   inputdir: ./data 
+   outputdir: ./output
+VarName: radial_velocity'
+inputfile: ufo_${OBSTYPE}_output_2020101300_0000.nc4
+OBSTYPE: rw
+subtask: 5
+EOF
+   
+python han.py
+#python han.py $taskdir/$filename $OBSTYPE $VarName $subtask
